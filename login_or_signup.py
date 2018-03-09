b1ecbae5cd0b57fef5355625dc12d41035a0807e
Chorken <36550575+Chorken@users.noreply.github.com>
2018-03-08 19:23:07 -0800

    Update login_or_signup.py

--- login_or_signup.py
+++ login_or_signup.py
@@ -1,28 +1,50 @@
 import math
 
-import Tkinter
+import tkinter
 
 import time
 
-username = raw_input('Enter username: ')
+cube = input('Do you have a cubeSurvivor account? Yes or no: ')
+if 'Yes':
+    time.sleep(1)
 
-if len(username) == 0:
+username = input('Enter your username: ')
+
+time.sleep(1)
+
+password = input('Enter your password: ')
+
+print('Logging in now...')
+
+if 'no' :
+    time.sleep(1)
+
+username = input('Enter username: ')
+
+while True:
+    (username) == 0
     time.sleep(0.5)
-    username = raw_input('Username must be greater than zero characters. Please enter a different username: ')
+    username = input('Username must be greater than zero characters. Please enter a different username: ')
 
 time.sleep(1)
 
-password = raw_input('Enter password: ')
+password = input('Enter password: ')
+
+if len(password) <= 8:
+    time.sleep(0.5)
+    password = input('Password must be at least eight characters. Please enter a different password: ')
 
 time.sleep(1)
 
-passwordc = raw_input('Confirm password: ')
+passwordc = input('Confirm password: ')
 
 if password != passwordc:
-   passwordc = raw_input('Please reconfirm password: ')
+    time.sleep(0.5)
+    passwordc = input('Please reconfirm password: ')
 
-email = raw_input('Enter your email: ')
+email = input('Enter your email: ')
 
 time.sleep(0.5)
 
 print('Account will be created within 24 hours.')
+
