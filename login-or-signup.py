19da0c56fc896632d39af9130c868bfa227b0635
Chorken <36550575+Chorken@users.noreply.github.com>
2018-12-18 18:20:21 -0800

    Update login-or-signup.py

--- login-or-signup.py
+++ login-or-signup.py
@@ -2,26 +2,26 @@ import math
 
 import time
 
-username = raw_input('Enter Username: ')
+username = input('Enter Username: ')
 
 while len(username) == 0:
     time.sleep(0.5)
-    username = raw_input('Username must be greater than zero characters. Please enter a different username: ')
+    username = input('Username must be greater than zero characters. Please enter a different username: ')
 
-password = raw_input('Enter password: ')
+password = input('Enter password: ')
 
 while len(password) <= 7:
     time.sleep(0.5)
-    password = raw_input('Password must be at least eight characters. Please enter a different password: ')
+    password = input('Password must be at least eight characters. Please enter a different password: ')
     time.sleep(1)
 
-passwordc = raw_input('Confirm password: ')
+passwordc = input('Confirm password: ')
     
 while len(password) != len(passwordc):
     time.sleep(0.5)
-    passwordc = raw_input('Please reconfirm password: ')
+    passwordc = input('Please reconfirm password: ')
 
-email = raw_input('Enter your email: ')
+email = input('Enter your email: ')
     
 time.sleep(0.5)
 
